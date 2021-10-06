a0bca44a892e1f54da8abd2ec7c7d5fdc4f2fde8
Student Account <danillosouza1704@gmail.com>
2021-10-06 20:27:17 -0300

    V1 - Distribuido

--- distribuido/src/socket.c
+++ distribuido/src/socket.c
@@ -13,66 +13,61 @@
 #include "cJSON.h"
 #include "gpio.h"
 #include "dht22.h"
-#include "json.h"
+#include "message_formater.h"
 
-// #define IP "127.0.0.1"
 #define IP "192.168.0.53"
 #define PORTA 10207
 #define MAX_SIZE 5000
 pthread_t SOCKET_GET, SOCKET_SEND;
 
-char *mensagem, *data;
-int clienteSocket;
-int temperature = 0, humidity = 0, toggle_value = 0;
+char *message, *data;
+int clientSocket;
+int temperature = 0, humidity = 0, total_people = 0, toggle_value = 0;
 
 void get_request()
 {
     int i;
     char num[5];
-    recv(clienteSocket, data, MAX_SIZE, 0);
-    perror("Teste1");
+    recv(clientSocket, data, MAX_SIZE, 0);
     for (i = 0; data[i] != '\0'; i++)
         if (i > 3)
             break;
-    printf("%s\n", data);
     if (i < 3)
     {
+        printf("Recebi porta: %d", atoi(num));
         toggle_value = 1;
         strcpy(num, data);
         int value = read_gpio(atoi(num));
-        printf("%s\n", num);
-        if(value == 0){
-            printf("Ligou\n");
+        if (value == 0)
+        {
+            printf("Ligando %d\n", atoi(num));
             turn_on(atoi(num));
-        } else {
-            printf("Desligou\n");
+        }
+        else
+        {
+            printf("Desligando%d\n", atoi(num));
             turn_off(atoi(num));
         }
     }
     else
     {
         toggle_value = 0;
-        strcpy(mensagem, data);
+        strcpy(message, data);
     }
     sleep(1);
 }
 
 void send_data()
 {
-    if (strlen(mensagem) > 0 && !toggle_value)
+    if (strlen(message) > 0 && !toggle_value)
     {
         char *final = malloc(MAX_SIZE);
-        perror("Teste 1");
-        Server *server_config = malloc(sizeof(Server) + 1);
-        perror("Teste 2");
-        parse_json_string(mensagem, server_config);
-        perror("Teste 3");
+        Server *server_config = malloc(sizeof(Server));
+        server_config->nome = malloc(20);
+        parse_json_string(message, server_config);
         read_dht_data(&temperature, &humidity, 0);
-        perror("Teste 4");
-        final_json(&final, server_config, temperature, humidity, -1);
-        perror("Teste 5");
-        send(clienteSocket, final, MAX_SIZE, 0);
-        perror("Teste 6");
+        final_json(&final, server_config, temperature, humidity, &total_people);
+        send(clientSocket, final, MAX_SIZE, 0);
         free(final);
     }
     sleep(1);
@@ -80,30 +75,26 @@ void send_data()
 
 void main_socket()
 {
-    struct sockaddr_in servidorAddr;
-    unsigned short servidorPorta;
-    char *IP_Servidor;
-    mensagem = calloc(MAX_SIZE, sizeof(char));
+    struct sockaddr_in serverAddr;
+    unsigned short serverPort;
+    char *IP_Server;
+    message = calloc(MAX_SIZE, sizeof(char));
     data = calloc(MAX_SIZE, sizeof(char));
-    IP_Servidor = inet_addr(IP);
-    servidorPorta = PORTA;
+    IP_Server = inet_addr(IP);
+    serverPort = PORTA;
 
-    if ((clienteSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    if ((clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
     {
         perror("Socket");
         exit(0);
     }
-    servidorAddr.sin_family = AF_INET;
-    servidorAddr.sin_addr.s_addr = IP_Servidor;
-    servidorAddr.sin_port = htons(servidorPorta);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = IP_Server;
+    serverAddr.sin_port = htons(serverPort);
 
-    if (connect(clienteSocket, (struct sockaddr *)&servidorAddr,
-                sizeof(servidorAddr)) < 0)
-    {
-        perror("Connect");
-        exit(0);
-    }
-    // get_request();
+    while (connect(clientSocket, (struct sockaddr *)&serverAddr,
+                sizeof(serverAddr)) < 0);
+
     while (1)
     {
         pthread_create(&SOCKET_GET, NULL, get_request, NULL);
